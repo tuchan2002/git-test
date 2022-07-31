25274be5467f01a5fd383ba78738dfbe2fac1dca
TranAnhTu17 <20020495@vnu.edu.vn>
2022-07-31 10:54:52 +0700

    feat: add eat for tiger

--- Tiger.ts
+++ Tiger.ts
@@ -1,11 +1,18 @@
 export class Tiger {
     private name: string;
     private age: number;
-    constructor(name: string, age: number) {
+    private mouth: boolean;
+    constructor(name: string, age: number, mouth: boolean) {
         this.name = name;
         this.age = age;
+        this.mouth = mouth;
     }
     print() {
-        console.log(`Name: ${this.name}, Age: ${this.age}`);
+        console.log(
+            `Name: ${this.name}, Age: ${this.age}, Mouth: ${this.mouth}`
+        );
+    }
+    eat() {
+        console.log(`${this.name} is eating...`);
     }
 }
