4c5a2f5e03b47c6e9d18508311b2f3d9eeede2ed
TranAnhTu17 <20020495@vnu.edu.vn>
2022-07-31 11:03:09 +0700

    feat: add swim for tiger

--- Tiger.ts
+++ Tiger.ts
@@ -1,11 +1,16 @@
 export class Tiger {
     private name: string;
     private age: number;
-    constructor(name: string, age: number) {
+    private arm: boolean;
+    constructor(name: string, age: number, arm: boolean) {
         this.name = name;
         this.age = age;
+        this.arm = arm;
     }
     print() {
-        console.log(`Name: ${this.name}, Age: ${this.age}`);
+        console.log(`Name: ${this.name}, Age: ${this.age}, Arm: ${this.arm}`);
+    }
+    swim() {
+        console.log(`${this.name} is swimming...`);
     }
 }
