9f2325bfc4dffb7d18ead097299c194f0f035745
TranAnhTu17 <20020495@vnu.edu.vn>
2022-07-31 15:49:06 +0700

    feat: add stomach for tiger

--- Tiger.ts
+++ Tiger.ts
@@ -3,19 +3,28 @@ export class Tiger {
     private age: number;
     private mouth: boolean;
     private arm: boolean;
-    constructor(name: string, age: number, mouth: boolean, arm: boolean) {
+    private stomach: string[];
+    constructor(
+        name: string,
+        age: number,
+        mouth: boolean,
+        arm: boolean,
+        stomach: string[]
+    ) {
         this.name = name;
         this.age = age;
         this.mouth = mouth;
         this.arm = arm;
+        this.stomach = stomach;
     }
     print() {
         console.log(
-            `Name: ${this.name}, Age: ${this.age}, Mouth: ${this.mouth}, Arm: ${this.arm}`
+            `Name: ${this.name}, Age: ${this.age}, Mouth: ${this.mouth}, Arm: ${this.arm}, Stomach: ${this.stomach}`
         );
     }
     eat() {
         console.log(`${this.name} is eating...`);
+        this.stomach.push("O");
     }
     swim() {
         console.log(`${this.name} is swimming...`);
