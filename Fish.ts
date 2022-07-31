4fe6dced8f5562bd517f01b0c88fc9c8c5d0f642
TranAnhTu17 <20020495@vnu.edu.vn>
2022-07-31 23:50:49 +0700

    feat: add stomach for fish

--- Fish.ts
+++ Fish.ts
@@ -1,11 +1,19 @@
 export class Fish {
     private name: string;
     private age: number;
-    constructor(name: string, age: number) {
+    private stomach: string[];
+    constructor(name: string, age: number, stomach: string[]) {
         this.name = name;
         this.age = age;
+        this.stomach = stomach;
     }
     print() {
-        console.log(`Name: ${this.name}, Age: ${this.age}`);
+        console.log(
+            `Name: ${this.name}, Age: ${this.age}, Stomach: ${this.stomach}`
+        );
+    }
+    eat() {
+        console.log(`${this.name} is eating...`);
+        this.stomach.push("O");
     }
 }
