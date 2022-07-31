6eb797bb906f0f041f81a6342176bd610eb4fa93
TranAnhTu17 <20020495@vnu.edu.vn>
2022-07-31 10:15:13 +0700

    feat: add hand for mouse

--- Mouse.ts
+++ Mouse.ts
@@ -3,23 +3,26 @@ export class Mouse {
     private age: number;
     private foot: boolean;
     private tooth: boolean;
+    private hand: boolean;
     private arm: boolean;
     constructor(
         name: string,
         age: number,
         foot: boolean,
         tooth: boolean,
+        hand: boolean,
         arm: boolean
     ) {
         this.name = name;
         this.age = age;
         this.foot = foot;
         this.tooth = tooth;
+        this.hand = hand;
         this.arm = arm;
     }
     print() {
         console.log(
-            `Name: ${this.name}, Age: ${this.age}, Tooth: ${this.tooth}, Foot: ${this.foot}, Arm: ${this.arm}`
+            `Name: ${this.name}, Age: ${this.age}, Tooth: ${this.tooth}, Foot: ${this.foot}, Arm: ${this.arm}, Hand: ${this.hand}`
         );
     }
     eat() {
@@ -27,6 +30,9 @@ export class Mouse {
     }
     run() {
         console.log(`${this.name} is running...`);
+    }
+    punch() {
+        console.log(`${this.name} is punching...`);
     }
     swim() {
         console.log(`${this.name} is swimming...`);
